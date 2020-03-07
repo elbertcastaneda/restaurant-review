37c1c0a827cac0e79272f867c495375a278b7899
Elbert Castañeda <elbert@sbxretail.com>
2020-03-06 22:13:41 -0600

    Base template v2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
 
-import componentsStyled from '../Components.styled';
+import cs from '../Components.styled';
 
-import styled from './App.styled';
-
-const { Code } = componentsStyled;
+import s from './App.styled';
 
 function App() {
   return (
-    <styled.Root>
-      <styled.Header>
-        <styled.Title>Hello world</styled.Title>
-        <styled.AppLogo />
+    <s.Root>
+      <s.Header>
+        <s.Title>Hello world</s.Title>
+        <s.AppLogo />
         <p>
-          Edit <Code>src/App</Code> and save to reload.
+          Edit <cs.Code>src/App</cs.Code> and save to reload.
         </p>
-        <styled.AppLink href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          <styled.Wrapper>Learn React</styled.Wrapper>
-        </styled.AppLink>
-      </styled.Header>
-    </styled.Root>
+        <s.AppLink href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          <s.Wrapper>Learn React</s.Wrapper>
+        </s.AppLink>
+      </s.Header>
+    </s.Root>
   );
 }
 
